97aa19f51db5968c456932b183d3d36a67aeef66
Andrew Kane <andrew@ankane.org>
2023-10-28 21:30:24 -0700

    Use assertions for test

--- src/test/java/com/pgvector/SpringJDBCTest.java
+++ src/test/java/com/pgvector/SpringJDBCTest.java
@@ -1,12 +1,17 @@
 package com.pgvector;
 
 import java.sql.*;
+import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import com.pgvector.PGvector;
+import org.postgresql.util.PGobject;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SpringJDBCTest {
     @Test
@@ -30,10 +35,17 @@ public class SpringJDBCTest {
 
         Object[] neighborParams = new Object[] { new PGvector(new float[] {1, 1, 1}) };
         List<Map<String, Object>> rows = jdbcTemplate.queryForList("SELECT * FROM spring_items ORDER BY embedding <-> ? LIMIT 5", neighborParams);
+        List<Long> ids = new ArrayList<>();
+        List<PGvector> embeddings = new ArrayList<>();
         for (Map row : rows) {
-            System.out.println(row.get("id"));
-            System.out.println(row.get("embedding"));
+            ids.add((Long) row.get("id"));
+            embeddings.add(row.get("embedding") == null ? null : new PGvector(((PGobject) row.get("embedding")).getValue()));
         }
+        assertArrayEquals(new Long[]{1L, 3L, 2L, 4L}, ids.toArray());
+        assertArrayEquals(new float[] {1, 1, 1}, embeddings.get(0).toArray());
+        assertArrayEquals(new float[] {1, 1, 2}, embeddings.get(1).toArray());
+        assertArrayEquals(new float[] {2, 2, 2}, embeddings.get(2).toArray());
+        assertNull(embeddings.get(3));
 
         jdbcTemplate.execute("CREATE INDEX ON spring_items USING ivfflat (embedding vector_l2_ops) WITH (lists = 100)");
     }
